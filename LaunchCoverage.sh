79c8c28505a84b6f714653793cb31512604b28d4
Edgar Cloud <fluent0x@gmail.com>
2021-02-09 13:54:41 -0500

    Add README/fix LaunchCoverage

--- LaunchCoverage.sh
+++ LaunchCoverage.sh
@@ -2,9 +2,8 @@
 
 PATH_TO_PROXYTCP=$1
 
-sudo cmake .
-sudo cmake --build .
 sudo cmake -DCMAKE_BUILD_TYPE=Debug .
+sudo cmake --build .
 
 ./bin/proxytcp_tests
 
@@ -16,8 +15,8 @@ mkdir CodeCoverage
 cd CodeCoverage
 mkdir html
 
-sudo lcov --rc lcov_branch_coverage=1 -d "../CMakeFiles/" --capture -o proxytcp.info -c
-sudo lcov --rc lcov_branch_coverage=1 --remove proxytcp.info "$PATH_TO_PROXYTCP/proxytcp/Tests/UnitTests/*" "$PATH_TO_PROXYTCP/proxytcp/googletest/*" "/usr/include/*" -o filtered.info
+sudo lcov -d "../CMakeFiles/" --capture -o proxytcp.info -c
+sudo lcov --remove proxytcp.info "$PATH_TO_PROXYTCP/proxytcp/Tests/UnitTests/*" "$PATH_TO_PROXYTCP/proxytcp/googletest/*" "/usr/include/*" -o filtered.info
 sudo genhtml -o html filtered.info
 
 cd ..
